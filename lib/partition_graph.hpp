0a5d041a7c1df61b8f475d1039b0967ff8854d3a
Isaac Arvestad <isaac.arvestad@gmail.com>
2020-09-04 14:09:59 +0200

    Template 'PartitionGraph' over vertex and edge type
    Work in progress. Will not build.

--- lib/partition_graph.hpp
+++ lib/partition_graph.hpp
@@ -5,15 +5,17 @@
 /**
    A graph which supports partitioning vertices into separate sub-graphs.
 
+   Parameterised over vertex type <V> and edge type <E>. An edge must have
+   members '.from' and '.to' with type <V>.
+
+
    Notation:
    - G=(V,E)
    - V = V_1 + V_2 + ... + V_k
    - E = E_1 + E_2 + ... + E_k
    - n = |V|, m = |E|, n_i = |V_i|, m_i = |E_i|
-
  */
-struct PartitionGraph {
-  using Vertex = int;
+template <typename V, typename E> struct PartitionGraph {
 
 private:
   int numPartitions;
@@ -30,19 +32,22 @@ private:
   /**
      Entire graph as a neighbor list.
    */
-  std::vector<std::vector<Vertex>> graph;
+  std::vector<std::vector<E>> graph;
 
   /**
      Subgraph where edges have been deleted if they belong to separate
      partitions.
    */
-  std::vector<std::vector<Vertex>> pGraph;
+  std::vector<std::vector<E>> pGraph;
 
 public:
   /**
      Construct an empty graph with a single partition.
    */
-  PartitionGraph(const int n);
+  PartitionGraph(const int n)
+      : numPartitions(1), numVertices(n), numEdges(0),
+        numEdgesInPartition(1, 0), numVerticesInPartition(1, n), partition(n),
+        graph(n), pGraph(n) {}
 
   /**
      Number of vertices in entire graph.
@@ -98,7 +103,25 @@ public:
 
      Time complexity: O(|xs|)
    */
-  int volume(const std::vector<Vertex> &xs) const;
+  template <typename It> int volume(const It &begin, const It &end) const {
+    int result = 0;
+    for (It it = begin; it != end; ++it)
+      result += degree(*it);
+    return result;
+  };
+
+  /**
+     The volume of a subset of nodes in their partition.
+
+     Time complexity: O(|xs|)
+   */
+  template <typename It>
+  int partitionVolume(const It &begin, const It &end) const {
+    int result = 0;
+    for (It it = begin; it != end; ++it)
+      result += partitionDegree(*it);
+    return result;
+  };
 
   /**
      The partition vertex 'u' is a part of.
@@ -108,21 +131,47 @@ public:
   int getPartition(const int u) const { return partition[u]; };
 
   /**
-     Neighbors of 'u' in entire graph.
+     Edges from 'u' in entire graph.
 
      Size of result: O(|E|)
      Time complexity: O(1)
    */
-  const std::vector<Vertex> &neighbors(const int u) const { return graph[u]; };
+  const std::vector<E> &edges(const int u) const { return graph[u]; };
 
   /**
-     Neighbours of 'u' in its partition.
+     Edges from 'u' in its partition.
 
      Size of result: O(|E_i|), where u \in V_i.
      Time complexity: O(1)
    */
-  const std::vector<Vertex> &partitionNeighbors(const int u) const {
-    return pGraph[u];
+  const std::vector<E> &partitionEdges(const int u) const { return pGraph[u]; };
+
+  /**
+     Edges from 'u' in entire graph.
+
+     Size of result: O(|E|)
+     Time complexity: O(1)
+   */
+  std::vector<V> neighbors(const int u) const {
+    std::vector<V> vs(graph[u].size());
+    for (int i = 0; i < (int)graph[u].size(); ++i)
+      vs[i] = graph[u].to;
+
+    return vs;
+  };
+
+  /**
+     Edges from 'u' in its partition.
+
+     Size of result: O(|E_i|), where u \in V_i.
+     Time complexity: O(1)
+   */
+  std::vector<V> partitionNeighbors(const int u) const {
+    std::vector<V> vs(pGraph[u].size());
+    for (int i = 0; i < (int)pGraph[u].size(); ++i)
+      vs[i] = pGraph[u].to;
+
+    return vs;
   };
 
   /**
@@ -130,7 +179,24 @@ public:
 
      Time complexity: O(1)
    */
-  void addEdge(const Vertex u, const Vertex v);
+  void addEdge(const E &e) {
+    V u = e.from, v = e.to;
+
+    assert(u < numVertices && v < numVertices && "Vertex out of bounds.");
+    if (u == v)
+      return;
+
+    numEdges++;
+
+    graph[u].push_back(v);
+    graph[v].push_back(u);
+    if (partition[u] == partition[v]) {
+      numEdgesInPartition[partition[u]]++;
+
+      pGraph[u].push_back(v);
+      pGraph[v].push_back(u);
+    }
+  }
 
   /**
      Create a new partition for the nodes 'xs' given the current nodes in the
@@ -141,6 +207,38 @@ public:
 
      Time complexity: O(|V_i| + |E_i|) where 'ys \subseteq V_i'
    */
-  int newPartition(const std::vector<Vertex> &xs,
-                   const std::vector<Vertex> &ys);
+  int newPartition(const std::vector<V> &xs, const std::vector<V> &ys) {
+    const int oldP = partition[xs[0]];
+    const int newP = numPartitions++;
+    for (const auto u : xs)
+      partition[u] = newP;
+
+    numEdgesInPartition.push_back(0);
+    numVerticesInPartition.push_back((int)xs.size());
+    numVerticesInPartition[oldP] -= numVerticesInPartition[newP];
+
+    for (const auto u : xs) {
+      pGraph[u].erase(std::remove_if(pGraph[u].begin(), pGraph[u].end(),
+                                     [&newP, this](const int v) {
+                                       return partition[v] != newP;
+                                     }),
+                      pGraph[u].end());
+      numEdgesInPartition[newP] += pGraph[u].size();
+    }
+    numEdgesInPartition[newP] /= 2; // Edges are double counted above
+
+    numEdgesInPartition[oldP] = 0;
+    for (const auto u : ys)
+      if (partition[u] == oldP) {
+        pGraph[u].erase(std::remove_if(pGraph[u].begin(), pGraph[u].end(),
+                                       [&oldP, this](const int v) {
+                                         return partition[v] != oldP;
+                                       }),
+                        pGraph[u].end());
+        numEdgesInPartition[oldP] += pGraph[u].size();
+      }
+    numEdgesInPartition[oldP] /= 2; // Edges are double counted above
+
+    return newP;
+  }
 };
