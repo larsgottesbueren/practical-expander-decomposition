46dc8ea28c0af8b2acc337655207b5e901c68df7
Isaac Arvestad <isaac.arvestad@gmail.com>
2020-09-04 16:55:10 +0200

    Use 'unique_ptr' in 'ExpanderDecomp'

--- lib/expander_decomp.cpp
+++ lib/expander_decomp.cpp
@@ -1,15 +1,15 @@
+#include <memory>
 #include <numeric>
 
 #include "expander_decomp.hpp"
 
-void ExpanderDecomp::compute(const std::vector<PartitionGraph::Vertex> &xs,
-                             int partition) {
+void ExpanderDecomp::compute(const std::vector<int> &xs, int partition) {
   CutMatching cm(graph, xs, partition, phi);
   auto result = cm.compute();
 
   switch (result.t) {
   case CutMatching::Balanced: {
-    int newPartition = graph.newPartition(result.a, xs);
+    int newPartition = graph->newPartition(result.a, xs);
     compute(result.a, newPartition);
     compute(result.r, partition);
     break;
@@ -23,18 +23,21 @@ void ExpanderDecomp::compute(const std::vector<PartitionGraph::Vertex> &xs,
   }
 }
 
-ExpanderDecomp::ExpanderDecomp(PartitionGraph &g, const double phi)
-    : graph(std::move(g)), phi(phi) {
-  std::vector<PartitionGraph::Vertex> vertices(g.size());
+ExpanderDecomp::ExpanderDecomp(const PartitionGraph<int, Edge> &g,
+                               const double phi)
+    : graph(std::make_unique<PartitionGraph<int, Edge>>(g)),
+      flowGraph(std::make_unique<UnitFlow>(g.size())),
+      subdivisionFlowGraph(std::make_unique<UnitFlow>(g.edgeCount())),
+      phi(phi) {
+
+  std::vector<int> vertices(g.size());
   std::iota(vertices.begin(), vertices.end(), 0);
   compute(vertices, 0);
 }
 
-std::vector<std::vector<PartitionGraph::Vertex>>
-ExpanderDecomp::getPartition() const {
-  std::vector<std::vector<PartitionGraph::Vertex>> result(
-      graph.partitionCount());
-  for (int u = 0; u < graph.size(); ++u)
-    result[graph.getPartition(u)].push_back(u);
+std::vector<std::vector<int>> ExpanderDecomp::getPartition() const {
+  std::vector<std::vector<int>> result(graph->partitionCount());
+  for (int u = 0; u < graph->size(); ++u)
+    result[graph->getPartition(u)].push_back(u);
   return result;
 }
--- lib/expander_decomp.hpp
+++ lib/expander_decomp.hpp
@@ -7,22 +7,23 @@
 
 struct ExpanderDecomp {
 private:
-  PartitionGraph graph;
+  std::unique_ptr<PartitionGraph<int, Edge>> graph;
+  std::unique_ptr<UnitFlow> flowGraph, subdivisionFlowGraph;
   const double phi;
 
   /**
      Compute expander decomposition for subset of vertices 'xs'.
    */
-  void compute(const std::vector<PartitionGraph::Vertex> &xs, int partition);
+  void compute(const std::vector<int> &xs, int partition);
 
 public:
   /**
      Create a decomposition problem with n vertices.
    */
-  ExpanderDecomp(PartitionGraph &g, const double phi);
+  ExpanderDecomp(const PartitionGraph<int, Edge> &g, const double phi);
 
   /**
      Return the computed partition as a vector of disjoint vertex vectors.
    */
-  std::vector<std::vector<PartitionGraph::Vertex>> getPartition() const;
+  std::vector<std::vector<int>> getPartition() const;
 };
