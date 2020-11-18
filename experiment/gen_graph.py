d829608c9afa80ae4b2d70524f4f3f90ca2c11e8
Isaac Arvestad <isaac.arvestad@gmail.com>
2020-11-18 13:55:21 +0100

    Improve command line api for 'gen_graph.py'

--- experiment/gen_graph.py
+++ experiment/gen_graph.py
@@ -1,41 +1,91 @@
 #!/usr/bin/env python3
-
 """Utility for creating various types of graphs.
 
 """
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Utility for creating graphs')
-parser.add_argument('graph_type', choices=['clique', 'dumbbell'], metavar='type')
-parser.add_argument('-n', type=int, default=100, help='number of vertices in graph (default=100)')
+def output(g,n,m):
+    problemInput = '{} {}'.format(n, m)
+    for u, vs in g.items():
+        for v in vs:
+            problemInput += '\n{} {}'.format(u, v)
+    print(problemInput)
 
-args = parser.parse_args()
+def gen_clique(args):
+    g = {}
+    n = args.n
+    k = args.k
+    m = 0
 
-g = {}
-n = args.n
-m = 0
-
-if args.graph_type == 'clique':
-    for u in range(n):
-        g[u] = []
-        for v in range(u+1,n):
-            g[u].append(v)
-            m += 1
-elif args.graph_type == 'dumbbell':
-    n1 = n // 2
-    for u in range(n):
-        g[u] = []
-        for v in range(u+1,n):
-            if (u < n1 and v < n1) or (u >= n1 and v >= n1):
+    for c in range(k):
+        for i in range(n):
+            u = c * n + i
+            g[u] = []
+            for j in range(i+1,n):
+                v = c * n + j
                 g[u].append(v)
                 m += 1
-    g[0].append(n1)
-    m += 1
 
-problemInput = '{} {}'.format(n, m)
-for u,vs in g.items():
-    for v in vs:
-        problemInput += '\n{} {}'.format(u, v)
+    output(g,n*k,m)
 
-print(problemInput)
+def gen_dumbbell(args):
+    g = {}
+    n = args.n
+    k = args.k
+    m = 0
+
+    for c in range(k):
+        for i in range(n):
+            u = c * n + i
+            g[u] = []
+            for j in range(i+1,n):
+                v = c * n + j
+                g[u].append(v)
+                m += 1
+
+    for c1 in range(k):
+        u = c1 * n
+        for c2 in range(c1+1,k):
+            v = c2 * n
+            g[u].append(v)
+            m += 1
+
+    output(g,n*k,m)
+
+parser = argparse.ArgumentParser(description='Utility for creating graphs')
+subparsers = parser.add_subparsers()
+
+clique_parser = subparsers.add_parser(
+    'clique',
+    help='k cliques, each with n vertices.')
+clique_parser.set_defaults(func=gen_clique)
+clique_parser.add_argument(
+    '-n',
+    type=int,
+    default=100,
+    help='number of vertices in each clique (default=100)')
+clique_parser.add_argument(
+    '-k',
+    type=int,
+    default=1,
+    help='number of cliques (default=1)')
+
+dumbbell_parser = subparsers.add_parser(
+    'dumbbell',
+    help='k subgraphs, each with n vertices, forming a clique'
+    )
+dumbbell_parser.set_defaults(func=gen_dumbbell)
+dumbbell_parser.add_argument(
+    '-n',
+    type=int,
+    default=100,
+    help='number of vertices in each clique (default=100)')
+dumbbell_parser.add_argument(
+    '-k',
+    type=int,
+    default=2,
+    help='number of cliques (default=2)')
+
+args = parser.parse_args()
+args.func(args)
