9eb767ef2ee40e3c3d0c611e4eb4e16ddd27095d
Isaac Arvestad <isaac.arvestad@gmail.com>
2020-11-26 08:54:29 +0100

    Use same argparsing as 'expander-decomp'
    Also output volume of the sides being cut.

--- main/cut.cpp
+++ main/cut.cpp
@@ -12,20 +12,27 @@
 
 using namespace std;
 
+DEFINE_double(
+    phi, 0.01,
+    "Value of \\phi such that expansion of each cluster is at least \\phi");
+DEFINE_int32(t1, 100, "Constant 't1' in 'T = t1 + t2 \\log^2 m'");
+DEFINE_double(t2, 1.0, "Constant 't2' in 'T = t1 + t2 \\log^2 m'");
+DEFINE_bool(one_indexed, false, "Vertices are 1-indexed instead of 0-indexed.");
+
 int main(int argc, char *argv[]) {
   google::InitGoogleLogging(argv[0]);
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-  double phi;
-  int tConst;
-  double tFactor;
   int n, m;
-  cin >> phi >> tConst >> tFactor >> n >> m;
+  cin >> n >> m;
   auto g = make_unique<Undirected::Graph>(n);
   for (int i = 0; i < m; ++i) {
     int u, v;
     cin >> u >> v;
-    g->addEdge(u, v);
+    if (FLAGS_one_indexed)
+      u--, v--;
+    if (u < v)
+      g->addEdge(u, v);
   }
 
   vector<int> xs(n);
@@ -35,10 +42,13 @@ int main(int argc, char *argv[]) {
       ExpanderDecomposition::constructSubdivisionFlowGraph(g);
 
   auto solver = CutMatching::Solver(flowGraph.get(), subdivisionFlowGraph.get(),
-                                    xs, phi, tConst, tFactor);
+                                    xs, FLAGS_phi, FLAGS_t1, FLAGS_t2);
   auto result = solver.compute();
+  flowGraph->newPartition(result.a, xs);
 
-  cout << result.a.size() << endl;
+  cout << flowGraph->volume(result.a.begin(), result.a.end()) << " "
+       << flowGraph->volume(result.r.begin(), result.r.end()) << endl
+       << result.a.size() << endl;
   for (int i = 0; i < (int)result.a.size(); ++i) {
     if (i != 0)
       cout << " ";
