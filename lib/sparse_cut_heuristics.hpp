3095f1f1f2ce6b5e0a83b0a9721bf7a3da5fed6a
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2023-10-27 16:10:42 +0200

    add Nibble impl

--- lib/sparse_cut_heuristics.hpp
+++ lib/sparse_cut_heuristics.hpp
@@ -80,3 +80,82 @@ private:
   std::vector<double> residual;
   std::vector<Vertex> queue;
 };
+
+class Nibble {
+public:
+  using Vertex = UnitFlow::Vertex;
+  double alpha = 0.15;
+  double epsilon = 1e-12;
+  double conductance_goal = 1e-3;
+
+  void SetGraph(UnitFlow::Graph& graph_) {
+    graph = &graph_;
+    ppr.SetGraph(graph_);
+    if (in_cut.size() < graph->size()) {
+      in_cut.resize(graph->size());
+    }
+    total_vol = graph->volume();
+  }
+
+  std::optional<std::vector<Vertex>> ComputeCut(Vertex seed) {
+    ppr.Compute(seed);
+    auto ppr_distr = ppr.ExtractSparsePageRankValues();
+    for (auto& pru : ppr_distr) {
+      pru.pr = pru.pr / graph->degree(pru.u);
+    }
+    std::sort(ppr_distr.begin(), ppr_distr.end(), [](const auto& l, const auto& r) { return l.pr > r.pr; });
+
+    double cut = 0;
+    double vol = 0;
+    double best_conductance = std::numeric_limits<double>::max();
+    int best_cut_index = -1;
+
+    for (int i = 0; i < ppr_distr.size(); ++i) {
+      const Vertex u = ppr_distr[i].u;
+      vol += graph->degree(u);
+
+      for (auto e = graph->beginEdge(u); e != graph->endEdge(u); ++e) {
+        Vertex v = e->to;
+        if (in_cut[v]) {
+          cut -= 1;
+        } else {
+          cut += 1;
+        }
+      }
+
+      const double conductance = cut / std::min(vol, total_vol - vol);
+      if (conductance < best_conductance) {
+        best_conductance = conductance;
+        best_cut_index = i;
+      }
+    }
+
+    if (best_conductance <= conductance_goal) {
+      std::vector<Vertex> cutset;
+      for (int i = 0; i <= best_cut_index; ++i) {
+        cutset.push_back(ppr_distr[i].u);
+      }
+      return cutset;
+    } else {
+      return std::nullopt;
+    }
+  }
+
+private:
+  UnitFlow::Graph *graph;
+  PersonalizedPageRank ppr;
+  std::vector<bool> in_cut;
+  double total_vol = 0.0;
+};
+
+class LocalSearch {
+
+};
+
+class SparseCutHeuristics {
+
+
+private:
+  Nibble nibble;
+  LocalSearch local_search;
+};
