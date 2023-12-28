3c3024987cafe579f09a163da70542598ff95a68
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2023-12-29 00:20:33 +0100

    add tlx for arg parser

--- main/edc.cpp
+++ main/edc.cpp
@@ -12,95 +12,110 @@
 #include <cut_matching.hpp>
 #include <datastructures/undirected_graph.hpp>
 #include <expander_decomp.hpp>
+#include <fstream>
+
+#include <tlx/cmdline_parser.hpp>
 
 int Logger::LOG_LEVEL = 0;
 
-std::unique_ptr<std::mt19937> configureRandomness(unsigned int seed) {
+std::unique_ptr<std::mt19937> configureRandomness(unsigned int seed)
+{
     std::mt19937 randomGen(seed);
     return std::make_unique<std::mt19937>(randomGen);
 }
 
-std::unique_ptr<Undirected::Graph> readGraph(bool chaco_format) {
+std::unique_ptr<Undirected::Graph> readGraph(const std::string& path)
+{
     int n, m;
-    std::cin >> n >> m;
+    std::ifstream in(path);
+    in >> n >> m;
 
     std::vector<Undirected::Edge> es;
-    if (chaco_format) {
-        std::cin.ignore();
-        for (int u = 0; u < n; ++u) {
-            std::string line;
-            std::getline(std::cin, line);
-            std::stringstream ss(line);
 
-            int v;
-            while (ss >> v)
-                if (u < --v)
-                    es.emplace_back(u, v);
-        }
-    } else {
-        std::set<std::pair<int, int>> seen;
-        for (int i = 0; i < m; ++i) {
-            int u, v;
-            std::cin >> u >> v;
-            if (u > v)
-                std::swap(u, v);
-            if (seen.find({u, v}) == seen.end()) {
-                seen.insert({u, v});
-                es.emplace_back(u, v);
-            }
+    std::set<std::pair<int, int>> seen;
+    for (int i = 0; i < m; ++i)
+    {
+        int u, v;
+        in >> u >> v;
+        if (u > v)
+            std::swap(u, v);
+        if (seen.find({u, v}) == seen.end())
+        {
+            seen.insert({u, v});
+            es.emplace_back(u, v);
         }
     }
 
     return std::make_unique<Undirected::Graph>(n, es);
 }
 
+int main(int argc, char* argv[])
+{
+    CutMatching::Parameters params = {
+        .tConst = 22, .tFactor = 5.0, .minIterations = 0, .minBalance = 0.45,
+        .samplePotential = false,
+        .balancedCutStrategy = true,
+        .use_cut_heuristics = true,
+        .use_potential_based_dynamic_stopping_criterion = true,
+        .num_flow_vectors = 20,
+        .tune_num_flow_vectors = false,
+    };
+    double phi = 0.001;
+    std::string graph_file;
+    int seed = 555;
+
+    tlx::CmdlineParser cp;
+    cp.set_description("Expander Decomposition");
+    cp.set_author("Isaac Arvestad and Lars Gottesbüren");
+    cp.add_int("log", Logger::LOG_LEVEL, "log level");
+    cp.add_double("phi", phi, "The conductance value");
+    cp.add_string('G', "graph", graph_file, "Path to the graph");
+    cp.add_int('S', "seed", seed, "Seed");
+
+    cp.add_bool("sample-potential", params.samplePotential, "Sample potentials [for debugging]");
+    cp.add_bool("tune-flow-vectors", params.tune_num_flow_vectors,
+        "Tune the number of flow vectors needed for good convergence speed [for debugging]");
+    cp.add_bool("use-cut-heuristics", params.use_cut_heuristics, "Try heuristic cut procedures before cut-matching.");
+    cp.add_bool("adaptive", params.use_potential_based_dynamic_stopping_criterion,
+        "Perform dynamic number of cut-matching rounds based on how well the flow vectors are mixing.");
 
 
-int main(int argc, char *argv[]) {
+    if (!cp.process(argc, argv))
+    {
+        std::exit(-1);
+    }
 
-  auto randomGen = configureRandomness(555);
 
-  if(const char* env_log_level = std::getenv("LOG_LEVEL")) {
-      int log_level = std::stoi(env_log_level);
-      Logger::LOG_LEVEL = log_level;
-  }
+    auto g = readGraph(graph_file);
 
-  auto g = readGraph(false);
+    auto randomGen = configureRandomness(seed);
 
-  CutMatching::Parameters params = {
-      .tConst = 22, .tFactor = 5.0, .minIterations = 0, .minBalance = 0.45,
-      .samplePotential = true,
-      .balancedCutStrategy = true,
-      .use_cut_heuristics = true,
-      .use_potential_based_dynamic_stopping_criterion = true,
-      .num_flow_vectors = 3,
-      .tune_num_flow_vectors = true,
-  };
-  double phi = 0.001;
+    Timer timer;
+    timer.Start();
+    ExpanderDecomposition::Solver solver(std::move(g), phi, randomGen.get(), params);
+    auto total_time = timer.Stop();
 
-  Timer timer; timer.Start();
-  ExpanderDecomposition::Solver solver(std::move(g), phi, randomGen.get(), params);
-  auto total_time = timer.Stop();
+    auto partitions = solver.getPartition();
+    auto conductances = solver.getConductance();
 
-  auto partitions = solver.getPartition();
-  auto conductances = solver.getConductance();
+    // Timings::GlobalTimings().Print();
+    // std::cout << "--- Total time " << total_time << " --- " << std::endl;
+    // std::cout << "--- Time for balanced cuts " << solver.time_balanced_cut << " time for expanders " << solver.time_expander << " ---" << std::endl;
 
-  // Timings::GlobalTimings().Print();
-  // std::cout << "--- Total time " << total_time << " --- " << std::endl;
-  // std::cout << "--- Time for balanced cuts " << solver.time_balanced_cut << " time for expanders " << solver.time_expander << " ---" << std::endl;
+    // std::cout << "Time pre excess " << solver.subdivisionFlowGraph->pre_excess << " time post excess " << solver.subdivisionFlowGraph->post_excess << std::endl;
+    if (params.tune_num_flow_vectors)
+    {
+        std::cout << "Num flow vectors" << solver.num_flow_vectors_needed << std::endl;
+    }
 
-  // std::cout << "Time pre excess " << solver.subdivisionFlowGraph->pre_excess << " time post excess " << solver.subdivisionFlowGraph->post_excess << std::endl;
-  if (params.tune_num_flow_vectors) {
-      std::cout << "Num flow vectors" << solver.num_flow_vectors_needed << std::endl;
-  }
+    return 0;
 
-  return 0;
-
-  std::cout << solver.getEdgesCut() << " " << partitions.size() << std::endl;
-  for (int i = 0; i < int(partitions.size()); ++i) {
-      std::cout << partitions[i].size() << " " << conductances[i];
-      for (auto p : partitions[i])
-          std::cout << " " << p;
-      std::cout << std::endl;
-  }
+    std::cout << solver.getEdgesCut() << " " << partitions.size() << std::endl;
+    for (int i = 0; i < int(partitions.size()); ++i)
+    {
+        std::cout << partitions[i].size() << " " << conductances[i];
+        for (auto p : partitions[i])
+            std::cout << " " << p;
+        std::cout << std::endl;
+    }
 }
