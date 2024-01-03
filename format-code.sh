9096607f5bf40dcb75092827eca60d41e66481c3
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2024-01-03 23:37:45 +0100

    clean up

---
+++ format-code.sh
@@ -0,0 +1,1 @@
+find lib/ main/ -iname *.h -o -iname *.cpp -o -iname *.cc -o -iname *.hpp | xargs clang-format -i --Wno-error=unknown
--- test.sh
+++
@@ -1,3 +0,0 @@
-bazel test --test_output=all --compilation_mode dbg //test:cluster_util_test --sandbox_debug
-# bazel test --config=default --test_output=all  //test:cluster_util_test
-# lldb ./bazel-bin/test/cluster_util_test -- --gtest_break_on_failure
